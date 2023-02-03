50aed7bdc908db94852db65e1e5a9b91f77524f5
abnerauad <abner.auad.santo@gmail.com>
2023-02-03 12:32:16 -0300

    Condition

---
+++ Aula11/node001.js
@@ -0,0 +1,10 @@
+var vel = 60
+
+console.log(`A velocidade do seu carro é de ${vel} KM/h`)
+
+if (vel > 60) { // Simple Condition
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+
+console.log('Dirija sempre com segurança!')
+
