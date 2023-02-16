2feb8299812fc2477b3cc1ecf5fed332b431ed44
abnerauad <abner.auad.santo@gmail.com>
2023-02-15 23:27:36 -0300

    ex contador

---
+++ Aula13-14/ex001.js
@@ -0,0 +1,27 @@
+//MANEIRAS DE FAZER REPETIÇÕES
+
+/* opção FOR */
+for (var a = 1; a <= 3; a++){
+    console.log('Oi')
+}
+
+/* opção DO-WHILE */
+var c = 1
+do {
+    console.log('oi')
+    c++
+} while (c<=3);
+
+
+/* MAIS USADO */
+var b = 1
+while (b <= 3){
+    console.log('oi')
+    b++
+}
+
+
+/* PIOR */
+console.log('oi')
+console.log('oi')
+console.log('oi')
